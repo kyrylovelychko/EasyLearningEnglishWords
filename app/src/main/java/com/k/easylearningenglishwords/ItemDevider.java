35124b0ae4bd13f64579cea26321bd53faef0c6d
kyrylovelychko <kirrikup@gmail.com>
2017-02-15 19:49:11 +0200

    Creating MainActivity. Creating DictionariesListFragment, DictionaryFragment, WordDetailsFragment. Creating ItemDivider. Creating layouts for this classes.

---
+++ app/src/main/java/com/k/easylearningenglishwords/ItemDevider.java
@@ -0,0 +1,10 @@
+package com.k.easylearningenglishwords;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+
+public class ItemDevider extends RecyclerView.ItemDecoration {
+    public ItemDevider(Context context) {
+
+    }
+}
